14c9dc826f506f3975ba27a52ba27eb5b5302610
jagjot29 <79759928+jagjot29@users.noreply.github.com>
2021-06-15 14:25:32 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"jagjotsingh.lalihiana_19@sakec.ac.in\"\n\
+email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
